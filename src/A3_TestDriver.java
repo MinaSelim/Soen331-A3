eb963eed8e62e06548411f425fba9a4fabb38c62
Mina <mina.selim2005@hotmail.com>
2019-03-11 02:53:45 -0400

    Added more stuff to test driver, and added ,project cause the existing one just breaks everything

--- src/A3_TestDriver.java
+++ src/A3_TestDriver.java
@@ -9,17 +9,23 @@ public class A3_TestDriver {
 		random.setSeed(3948902489032L);
 		PriorityQueue queue = new PriorityQueue(10);
 		
-		queue.min();
-		queue.remove();
+		System.out.println("calling min : " + queue.min());
+		System.out.println("calling remove : " + queue.remove());
 
 		for(Integer i = 0; i<15; i++) //populate the queue
 		{
 			queue.insert(random.nextInt(200), i.toString());
 		}
-		queue.printHeap(System.out); 
+		
+		queue.printHeap(System.out); // Prints the keys in the heap in a tree-like format 
 		
 		System.out.println("calling min : " + queue.min());
 
+		for(Integer i = 0; i<15; i++) //empty the queue
+		{
+			String item = queue.remove();
+			System.out.println("removed item: " + item); //reminder that the items are just the counters from 1-10
+		}
 	}
 
 }
